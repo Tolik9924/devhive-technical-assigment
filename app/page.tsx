ef7f9f2703d385c316bab965f51d33e9233c306d
Tolik9924 <sakurasoft9924@gmail.com>
2026-02-05 15:05:22 +0200

    add description for readme

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,6 @@ import { UsersClient } from "@/components/users/UsersClient/UsersClient";
 
 import styles from "./page.module.css";
 
-/**
- * Server Component
- * - Responsible ONLY for data fetching
- * - No client state or interactivity here
- */
-
 const UsersPage = async () => {
   return (
     <div className={styles.usersPage}>
